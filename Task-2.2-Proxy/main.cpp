93479b6230e9d83a6c7fda742c4329205fce5d44
Margaret Alade <alade.margaret@yandex.ru>
2024-07-31 06:40:26 +0300

    Initial Commit

---
+++ Task-2.2-Proxy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Task-2.2-Proxy
+//
+//  Created by Маргарет  on 31.07.2024.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
